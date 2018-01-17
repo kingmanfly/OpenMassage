4b2c522ece58bef4ea435bad337eca67b9cf068f
lijinwei <lijinwei_123@126.com>
2018-01-17 10:35:31 +0800

    验证码，注册，忘记密码功能完成

--- pages/register/register.js
+++ pages/register/register.js
@@ -1,6 +1,7 @@
 // pages/register/register.js
 var util = require('../../utils/util.js');
 var app = getApp();
+var countdown = 60;
 
 Page({
 
@@ -11,7 +12,10 @@ Page({
     // fromType 1. 来自注册，2. 来自忘记密码
     fromType: null,
     verifyCode: null,
-    passwordTag: true
+    passwordTag: true,
+    enableGet: true,
+    countDownSecond: "60",
+    userPhone: null
   },
 
   /**
@@ -26,77 +30,168 @@ Page({
       wx.setNavigationBarTitle({
         title: '忘记密码',
       })
-    }else{
-      this.setData({ fromType: FROM_REGISTER});
+    } else {
+      this.setData({ fromType: FROM_REGISTER });
     }
   },
-  onSendVerifyCode:function(){
+  userPhoneInput: function (e) {
     this.setData({
-      verifyCode:1111
+      userPhone: e.detail.value
     })
   },
-  formSubmit: function(event){
-    console.log(event);
-    if(this.checkFormSuccess(event)){
-      console.log("checkFormSuccess"); 
-      let _this = this;
-      wx.request({
-        url: app.globalData.urlBase2 + 'user/register/',
-        method: 'POST',
-        data:{
-          phone: util.trim(event.detail.value.phone, 'all'),
-          password: event.detail.value.password,
-          checkcode:  1111,
-          neckname: event.detail.value.nick
-        },
-        header: {
-          'content-type': 'application/x-www-form-urlencoded'
-        },
-        success: function (res) {
-          console.log(res.data);
-          if(res.data.code == 1){
-            wx.showToast({
-              title: '注册成功',
-              icon: 'success',
-              duration: 3000
-            });
-            setTimeout(function () {
-              wx.navigateTo({
-                url: '/pages/login/login',
-              });
-            }, 3000)
-          }else{
-            wx.showToast({
-              title: '注册' + res.data.failureDetail,
-              icon: 'success',
-              duration: 3000
-            });
-          }
-        },
-        fail: function (res) {
-          console.log(res);
-        }
+  onSendVerifyCode: function () {
+    var that = this;
+    this.setData({
+      enableGet: (!that.data.enableGet)
+    });
+    that.settime(that);
+    var phone = util.trim(that.data.userPhone,"all");
+    if (phone == null || phone.length != 11){
+      wx.showToast({
+        title: '请输入手机号',
+        icon: 'fail',
+        duration: 3000
+      });
+      return;
+    }
+    wx.request({
+      url: app.globalData.urlBase2 + 'user/getVerifyCode/',
+      method: 'POST',
+      data: {
+        phone: phone
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      success: function(res){
+        console.log(res);
+      },
+      fail: function(res){
+        console.log(res);
+      }
+    })
+  },
+
+  settime: function (that) {
+    if (countdown == 0) {
+      that.setData({
+        enableGet: true
       })
+      countdown = 60;
+      return;
+    } else {
+      that.setData({
+        enableGet: false,
+        countDownSecond: countdown
+      });
+      countdown--;
+    }
+    setTimeout(function () {
+      that.settime(that)
+    }, 1000)
+  },
+  formSubmit: function (event) {
+    console.log(event);
+    if (this.checkFormSuccess(event)) {
+      console.log("checkFormSuccess");
+      let _this = this;
+      if (this.data.fromType == 1){
+        wx.request({
+          url: app.globalData.urlBase2 + 'user/register/',
+          method: 'POST',
+          data: {
+            phone: util.trim(event.detail.value.phone, 'all'),
+            password: event.detail.value.password,
+            checkcode: event.detail.value.validCode,
+            nickname: event.detail.value.nickname
+          },
+          header: {
+            'content-type': 'application/x-www-form-urlencoded'
+          },
+          success: function (res) {
+            console.log(res.data);
+            if (res.data.code == 1) {
+              wx.showToast({
+                title: '注册成功',
+                icon: 'success',
+                duration: 3000
+              });
+              setTimeout(function () {
+                wx.navigateTo({
+                  url: '/pages/login/login',
+                });
+              }, 3000)
+            } else {
+              wx.showToast({
+                title: '注册' + res.data.failureDetail,
+                icon: 'success',
+                duration: 3000
+              });
+            }
+          },
+          fail: function (res) {
+            console.log(res);
+          }
+        });
+      }else{
+        wx.request({
+          url: app.globalData.urlBase2 + 'user/passwordBack/',
+          method: 'POST',
+          data: {
+            phone: util.trim(event.detail.value.phone, 'all'),
+            password: event.detail.value.password,
+            checkcode: event.detail.value.validCode
+          },
+          header: {
+            'content-type': 'application/x-www-form-urlencoded'
+          },
+          success: function (res) {
+            console.log(res.data);
+            if (res.data.code == 1) {
+              wx.showToast({
+                title: '重置密码成功',
+                icon: 'success',
+                duration: 3000
+              });
+              setTimeout(function () {
+                wx.navigateTo({
+                  url: '/pages/login/login',
+                });
+              }, 3000)
+            } else {
+              wx.showToast({
+                title: '重置失败 ' + res.data.failureDetail,
+                icon: 'success',
+                duration: 3000
+              });
+            }
+          },
+          fail: function (res) {
+            console.log(res);
+          }
+        })
+      }
+      
     }
   },
-  checkFormSuccess: function(event){
-    var result = util.trim(event.detail.value.phone,'all');
+  checkFormSuccess: function (event) {
+    var result = util.trim(event.detail.value.phone, 'all');
     console.log("result=" + result);
     if (result.length != 11) {
       wx.showToast({
-        title: '手机号格式不正确',
+        title: '手机格式不正确',
         icon: 'success',
         duration: 2000
       })
       return false;
-    } else if (event.detail.value.validCode != '1111'){
+    } else if (event.detail.value.validCode.length != 4) {
       wx.showToast({
         title: '验证码不正确',
         icon: 'success',
         duration: 2000
       })
       return false;
-    }else if (event.detail.value.password != event.detail.value.passwordConfirm) {
+    } else if (event.detail.value.password != event.detail.value.passwordConfirm) {
       wx.showToast({
         title: '密码输入不一致',
         icon: 'success',
@@ -106,7 +201,7 @@ Page({
     }
     return true;
   },
-  onTogglePasswordViewStatus : function () {
+  onTogglePasswordViewStatus: function () {
     this.setData({
       passwordTag: !this.data.passwordTag
     });
